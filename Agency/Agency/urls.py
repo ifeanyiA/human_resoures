328946a8083cf8eb58f101e2f7a5d4e6098d009d
ifeanyi anaga <ifeanyianaga95@gmail.com>
2023-06-21 23:51:10 +0100

    connect to data

---
+++ Agency/Agency/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from App import views
+
+urlpatterns = [
+    #path to access django admin
+    path('admin/', admin.site.urls),
+    # path to render homepage
+    path('',views.home,name="home")
+]
