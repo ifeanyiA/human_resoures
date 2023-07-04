26d4cc7f9f3164fae858fc8f176b2ac34ff9d8a7
ifeanyi anaga <ifeanyianaga95@gmail.com>
2023-07-04 17:03:06 +0100

    frontend modal validation

--- App/static/js/script.js
+++ App/static/js/script.js
@@ -22,5 +22,44 @@ function validateEmail(email){
 
 }
 function validateForm(){
-    
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+    const phone = document.getElementById("phone").value;
+    const email = document.getElementById("email").value;
+    const address = document.getElementById("address").value;
+    const experience = document.getElementById("experience").value;
+    const skills = document.getElementById("skills").value;
+    const file = document.getElementById("file").value;
+
+    if(name == ""){
+        swal("Opsss !","Name field cannot be empty.","error");
+        return false;
+    }
+    else if(age == ""){
+        swal("Opsss !","Age field cannot be empty.","error");
+        return false;
+    }
+    else if(email == ""){
+        swal("Opsss !","Email field cannot be empty.","error");
+        return false;
+    }
+    else if(phone == ""){
+        swal("Opsss !","Phone field cannot be empty.","error");
+        return false;
+    }
+    else if(experience == ""){
+        swal("Opsss !","Experience field cannot be empty.","error");
+        return false;
+    }
+    else if(skills == ""){
+        swal("Opsss !","Skills field cannot be empty.","error");
+        return false;
+    }
+    else if(file == ""){
+        swal("Opsss !","File field cannot be empty.","error");
+        return false;
+    }
+    else{
+        return true;
+    }
 }
